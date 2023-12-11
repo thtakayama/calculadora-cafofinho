9baf7884e74254b9fb0b3bcf6fb8ed76b4e3d456
Thales Takayama <th.takayama58@gmail.com>
2023-12-11 08:13:23 -0300

    Otimização das funções para funcionar em valores gerais ao invés de valores específicos, evitando repetição de código

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 "use strict";
 
-console.log("teste");
+const btnMoradia = document.querySelector("#moradia .btn");
+let valoresMoradia = document.querySelectorAll("#moradia .campo input");
+let totalMoradia = document.querySelector("#moradia h1.total");
+
+const listar = (lista) => {
+  let valores = [];
+  lista.forEach((item) => {
+    valores.push(Number(item.value));
+    console.log(valores);
+  });
+  return valores;
+};
+
+const adicionarVirgula = (texto, valor) => {
+  texto.textContent = valor.toFixed(2).replace(".", ",");
+};
+
+btnMoradia.addEventListener("click", function (e) {
+  //previne o evento padrão do formulário
+  e.preventDefault();
+  //criar uma array vazia
+  let valores = listar(valoresMoradia);
+  console.log(valores);
+  let total = valores.reduce((acc, cur) => cur + acc, 0);
+  console.log(total);
+  adicionarVirgula(totalMoradia, total);
+  // totalMoradia.textContent = total.toFixed(2).replace(".", ",");
+});

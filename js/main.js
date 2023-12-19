75ffe9929037c26e2470a80967b24545a7ca9b6a
Thales Takayama <th.takayama58@gmail.com>
2023-12-19 08:34:30 -0300

    Criar um função reutilizável para calcular as diferentes categorias

--- js/main.js
+++ js/main.js
@@ -17,14 +17,18 @@ const adicionarVirgula = (texto, valor) => {
   texto.textContent = valor.toFixed(2).replace(".", ",");
 };
 
-btnMoradia.addEventListener("click", function (e) {
-  //previne o evento padrão do formulário
-  e.preventDefault();
-  //criar uma array vazia
-  let valores = listar(valoresMoradia);
-  console.log(valores);
-  let total = valores.reduce((acc, cur) => cur + acc, 0);
-  console.log(total);
-  adicionarVirgula(totalMoradia, total);
-  // totalMoradia.textContent = total.toFixed(2).replace(".", ",");
-});
+const calcular = (btn, listaValores, resultado) => {
+  btn.addEventListener("click", function (e) {
+    //previne o evento padrão do formulário
+    e.preventDefault();
+    //criar uma array com elementos do tipo número
+    let valores = listar(listaValores);
+    //Calcular o total da array acima
+    let total = valores.reduce((acc, cur) => cur + acc, 0);
+    //Substituir ponto por vírgula no total calculado acima
+    adicionarVirgula(resultado, total);
+  });
+};
+
+//Calculaar categoria Moradia
+calcular(btnMoradia, valoresMoradia, totalMoradia);
